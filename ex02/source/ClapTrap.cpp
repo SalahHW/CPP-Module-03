b3a43b0b86b3348cfe49fe99abb9974cf03b9c00
Salah <salah.bouheniche@gmail.com>
2023-12-06 14:23:10 +0100

    Remove in class initialization

--- ex02/source/ClapTrap.cpp
+++ ex02/source/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 20:03:49 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/12/03 12:25:33 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/12/06 14:22:19 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,12 @@ ClapTrap::ClapTrap()
 	std::cout << "ClapTrap constructed" << std::endl;	
 }
 
-ClapTrap::ClapTrap(std::string name) : name(name) 
+ClapTrap::ClapTrap(std::string name)
 {
+	this->name = name;
+	this->hit_point = 10;
+	this->energy_point = 10;
+	this->attack_damage = 0;
 	std::cout << "ClapTrap " << name << " constructed" << std::endl;
 }
 
